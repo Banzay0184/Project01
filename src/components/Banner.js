35e6fa45b5db5d33148d0be1bc3c28daee8487a7
Banzay0184 <shakhzodabidov@gmail.com>
2024-04-06 17:50:30 +0500

    0.09

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,76 +1,130 @@
-import { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import {useState, useEffect} from "react";
+import {Container, Row, Col} from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
+import {ArrowRightCircle} from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
 export const Banner = () => {
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
-  const period = 2000;
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
+    const toRotate = ["Личное обучение профессиональному тренингу",];
+    const period = 2000;
 
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
 
-    return () => { clearInterval(ticker) };
-  }, [text])
+        return () => {
+            clearInterval(ticker)
+        };
+    }, [text])
 
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
-    setText(updatedText);
+        setText(updatedText);
 
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
+        } else {
+            setIndex(prevIndex => prevIndex + 1);
+        }
     }
 
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
-      setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
-      setDelta(500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
-    }
-  }
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="aligh-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <TrackVisibility>
+                            {({isVisible}) =>
+                                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                                    <h1><span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Личное обучение профессиональному тренингу"]'><span
+                                        className="wrap">{text}</span></span></h1>
+                                    <div className="">
+                                        <p>Обо мне</p>
+                                        <ul className='about-list'>
+                                            <li>
+                                                <svg width="18.000000" height="12.000000" viewBox="0 0 18 12"
+                                                     fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path id="Форма 1"
+                                                          d="M2 5.5L5.83 8.91C6.21 9.24 6.78 9.24 7.16 8.91L15 2"
+                                                          stroke="#FFFFFF" stroke-opacity="1.000000"
+                                                          stroke-width="4.000000" stroke-linecap="round"/>
+                                                </svg>Заработал первый миллион рублей в 16 лет.
+                                            </li>
+                                            <li>
+                                                <svg width="18.000000" height="12.000000" viewBox="0 0 18 12"
+                                                     fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path id="Форма 1"
+                                                          d="M2 5.5L5.83 8.91C6.21 9.24 6.78 9.24 7.16 8.91L15 2"
+                                                          stroke="#FFFFFF" stroke-opacity="1.000000"
+                                                          stroke-width="4.000000" stroke-linecap="round"/>
+                                                </svg>В сфере трейдинга с 13 лет.
 
-  return (
-    <section className="banner" id="home">
-      <Container>
-        <Row className="aligh-items-center">
-          <Col xs={12} md={6} xl={7}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
-              </div>}
-            </TrackVisibility>
-          </Col>
-          <Col xs={12} md={6} xl={5}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={headerImg} alt="Header Img"/>
-                </div>}
-            </TrackVisibility>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-  )
+                                            </li>
+                                            <li>
+                                                <svg width="18.000000" height="12.000000" viewBox="0 0 18 12"
+                                                     fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path id="Форма 1"
+                                                          d="M2 5.5L5.83 8.91C6.21 9.24 6.78 9.24 7.16 8.91L15 2"
+                                                          stroke="#FFFFFF" stroke-opacity="1.000000"
+                                                          stroke-width="4.000000" stroke-linecap="round"/>
+                                                </svg>Вот уже как 5 лет я торгую и зарабатываю на финансовых рынках. В чем помогу и тебе!.
+                                            </li>
+                                            <li>
+                                                <svg width="18.000000" height="12.000000" viewBox="0 0 18 12"
+                                                     fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path id="Форма 1"
+                                                          d="M2 5.5L5.83 8.91C6.21 9.24 6.78 9.24 7.16 8.91L15 2"
+                                                          stroke="#FFFFFF" stroke-opacity="1.000000"
+                                                          stroke-width="4.000000" stroke-linecap="round"/>
+                                                </svg>Торгую любые рынки и любой таймфрейм (форекс, акции, фьючерсы).
+                                            </li>
+                                            <li>
+                                                <svg width="18.000000" height="12.000000" viewBox="0 0 18 12"
+                                                     fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path id="Форма 1"
+                                                          d="M2 5.5L5.83 8.91C6.21 9.24 6.78 9.24 7.16 8.91L15 2"
+                                                          stroke="#FFFFFF" stroke-opacity="1.000000"
+                                                          stroke-width="4.000000" stroke-linecap="round"/>
+                                                </svg>Совокупность моих активов превысила сумму в 350.000$ на момент 2024 года.
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <button onClick={() => console.log('connect')}>Узнать больше обо мне <ArrowRightCircle
+                                        size={25}/></button>
+                                </div>}
+                        </TrackVisibility>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <TrackVisibility>
+                            {({isVisible}) =>
+                                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                                    <img src={headerImg} alt="Header Img"/>
+                                </div>}
+                        </TrackVisibility>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import { ProjectCard } from "./ProjectCard";
+import {Container, Row, Col, Tab, Nav} from "react-bootstrap";
+import {ProjectCard} from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
@@ -10,89 +10,107 @@ import colorSharp from "../assets/img/color-sharp.png";
 
 export const Projects = () => {
 
-  const projects = [
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-  ];
+    const projects = [
+        {
+            title: "Понятие рыночного контекста",
+            description: "",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Как определить крупного игрока в рынке",
+            description: "",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Сила / слабость рынка",
+            description: "",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Уровни. Как работает от уровней",
+            description: "",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Торговый сетапы (точка входа)",
+            description: "",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Введение позиций. stop-loss",
+            description: "",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Риск и мани-менеджмент",
+            description: "",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Торговый алгоритм",
+            description: "",
+            imgUrl: projImg3,
+        },
+    ];
 
-  return (
-    <section className="project" id="projects">
-      <Container>
-        <Row>
-          <Col size={12}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-              <div id='styl' className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
-            </TrackVisibility>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-  )
+    return (
+        <section className="project" id="projects">
+            <Container>
+                <Row>
+                    <Col size={12}>
+                        <TrackVisibility>
+                            {({isVisible}) =>
+                                <div id='styl' className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                                    <h2>Программа обучения</h2>
+                                    <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                                        <Nav variant="pills"
+                                             className="nav-pills mb-5 justify-content-center align-items-center"
+                                             id="pills-tab">
+                                            <Nav.Item>
+                                                <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                            </Nav.Item>
+                                            <Nav.Item>
+                                                <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                            </Nav.Item>
+                                            <Nav.Item>
+                                                <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                                            </Nav.Item>
+                                        </Nav>
+                                        <Tab.Content id="slideInUp"
+                                                     className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                                            <Tab.Pane eventKey="first">
+                                                <Row>
+                                                    {
+                                                        projects.map((project, index) => {
+                                                            return (
+                                                                <ProjectCard
+                                                                    key={index}
+                                                                    {...project}
+                                                                />
+                                                            )
+                                                        })
+                                                    }
+                                                </Row>
+                                            </Tab.Pane>
+                                            <Tab.Pane eventKey="section">
+                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam,
+                                                    quod neque provident velit, rem explicabo excepturi id illo
+                                                    molestiae blanditiis, eligendi dicta officiis asperiores delectus
+                                                    quasi inventore debitis quo.</p>
+                                            </Tab.Pane>
+                                            <Tab.Pane eventKey="third">
+                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam,
+                                                    quod neque provident velit, rem explicabo excepturi id illo
+                                                    molestiae blanditiis, eligendi dicta officiis asperiores delectus
+                                                    quasi inventore debitis quo.</p>
+                                            </Tab.Pane>
+                                        </Tab.Content>
+                                    </Tab.Container>
+                                </div>}
+                        </TrackVisibility>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
 }
